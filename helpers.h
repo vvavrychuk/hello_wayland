c70e2ba37034a1a09ea07e56a359cd8bbc99b7c9
Henrique Dante de Almeida <hdante@gmail.com>
2014-07-05 19:08:51 -0300

    Change coding style for blogging
    - line length is 64 columns
    - no tabs
    - indent is 4 spaces
    - continuation lines are 1 indent
    - no newline after open parentheses
    - no newline before close parentheses

--- helpers.h
+++ helpers.h
@@ -15,8 +15,10 @@ typedef uint32_t pixel;
 void hello_setup_wayland(void);
 void hello_cleanup_wayland(void);
 
-struct wl_buffer *hello_create_buffer(struct wl_shm_pool *pool, unsigned width, unsigned height);
-void hello_bind_buffer(struct wl_buffer *buffer, struct wl_shell_surface *shell_surface);
+struct wl_buffer *hello_create_buffer(struct wl_shm_pool *pool,
+    unsigned width, unsigned height);
+void hello_bind_buffer(struct wl_buffer *buffer,
+    struct wl_shell_surface *shell_surface);
 void hello_free_buffer(struct wl_buffer *buffer);
 
 struct wl_shm_pool *hello_create_memory_pool(int file);
@@ -24,9 +26,11 @@ void hello_free_memory_pool(struct wl_shm_pool *pool);
 struct wl_shell_surface *hello_create_surface(void);
 void hello_free_surface(struct wl_shell_surface *surface);
 
-void hello_set_cursor_from_pool(struct wl_shm_pool *pool, unsigned width, unsigned height,
-				int32_t hot_spot_x, int32_t hot_spot_y);
+void hello_set_cursor_from_pool(struct wl_shm_pool *pool,
+    unsigned width, unsigned height,
+    int32_t hot_spot_x, int32_t hot_spot_y);
 void hello_free_cursor(void);
-void hello_set_button_callback(struct wl_shell_surface *surface, void (*callback)(uint32_t));
+void hello_set_button_callback(struct wl_shell_surface *surface,
+    void (*callback)(uint32_t));
 
 #endif /* _HELPERS_H_ */
